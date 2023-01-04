1aaa8fd5878ca9a4cea7087b837c918419f08280
robert <robert@masikh.org>
2023-01-04 17:48:51 +0100

    decoder output for two pico's

--- python-test/byte-beat-json-decoder.py
+++ python-test/byte-beat-json-decoder.py
@@ -2,24 +2,29 @@ import json
 import serial
 import time
 
-
+# Set serial device
 ser = serial.Serial('/dev/tty.usbserial-A50285BI', 115200, timeout=0.5)
 
 
 def update():
     while True:
+        # Tic Toc, performance counter
         tic = time.perf_counter()
+
+        # To pico 0
         ser.write(48).to_bytes(1, byteorder='little')
         input0 = ser.read_until()
         message0 = json.loads(input0.decode('utf-8'))
 
+        # To pico 1
         ser.write(49).to_bytes(1, byteorder='little')
         input1 = ser.read_until()
         message1 = json.loads(input1.decode('utf-8'))
 
         toc = time.perf_counter()
 
-        print(f'{message0} SR-PI: {1/(toc - tic):0.4f}/seconds                        \r', end='')
+        # standard out
+        print(f'{message0} {message1} SR-PI: {1/(toc - tic):0.4f}/seconds                        \r', end='')
 
 
 if __name__ == '__main__':
