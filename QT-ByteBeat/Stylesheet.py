43f8d4c6b2533863ef76c90a802be6fe40195ae2
robert <robert@masikh.org>
2023-01-08 18:51:56 +0100

    QT interface

---
+++ QT-ByteBeat/Stylesheet.py
@@ -0,0 +1,102 @@
+stylesheet = """
+#background {
+    background-image: url(bg:assets/aluminium_texture.png);
+    background-repeat: no-repeat;
+    background-position: center;
+}
+
+#frequency_plot {
+    background-image: url(bg:assets/nebula_top.png);
+    background-repeat: no-repeat;
+    background-position: center;
+}
+
+#formula_selector {
+    background-image: url(bg:assets/nebula_bottom.png);
+    background-repeat: no-repeat;
+    background-position: center;
+}
+
+#potmeter_1 {
+    background-color: orange;
+}
+
+#potmeter_2 {
+    background-color: blue;
+}
+
+#potmeter_3 {
+    background-color: green;
+}
+
+#potmeter_4 {
+    background-color: red;
+}
+
+#button_right {
+    border: 12px solid black;
+    background-color: red;
+    border-radius: 55;
+}
+
+#button_left {
+    border: 12px solid black;
+    background-color: red;
+    border-radius: 55;
+}
+
+#button_1 {
+    border: 8px solid darkorange;
+    background-color: gray;
+    border-radius: 10px;
+}
+
+#button_1:enabled {
+    border: 8px solid darkorange;
+    background-color: orange;
+    border-radius: 10px;
+}
+
+
+#button_2 {
+    border: 8px solid darkblue;
+    background-color: gray;
+    border-radius: 10px;
+}
+
+#button_2:enabled {
+    border: 8px solid darkblue;
+    background-color: blue;
+    border-radius: 10px;
+}
+
+#button_3 {
+    border: 8px solid darkgreen;
+    background-color: gray;
+    border-radius: 10px;
+}
+
+#button_3:enabled {
+    border: 8px solid darkgreen;
+    background-color: green;
+    border-radius: 10px;
+}
+
+#button_4 {
+    border: 8px solid darkred;
+    background-color: gray;
+    border-radius: 10px;
+}
+
+#button_4:enabled {
+    border: 8px solid darkred;
+    background-color: red;
+    border-radius: 10px;
+}
+
+QLineEdit
+{
+  color: black;
+  padding: 12px;
+}
+"""
