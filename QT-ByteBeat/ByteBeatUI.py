1102f4d75e9794386ffc44b57eb40af22f82ec77
robert <robert@masikh.org>
2023-01-21 01:35:24 +0100

    Added playout function
    Calling PlayByteBeat.py
    Set values from pods and buttons in formula (realtime)

--- QT-ByteBeat/ByteBeatUI.py
+++ QT-ByteBeat/ByteBeatUI.py
@@ -21,7 +21,58 @@ GPIO.setup(left_button_gpio, GPIO.IN)
 GPIO.setup(right_button_gpio, GPIO.IN)
 
 
-class Worker(QObject):
+class WorkerPlayer(QObject):
+    finished = pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        # PlayByteBeat class
+        self.byte_beat = PlayByteBeat()
+
+        # Current en next Formula
+        self.byte_beat.current_formula = ''
+        self.byte_beat.next_formula = ''
+
+        # Current formula is byte or float beat
+        self.is_byte_beat = True
+
+        # Play states
+        self.play = False
+        self.byte_beat.t = 1
+
+    def switch_play_pause(self):
+        if self.byte_beat.current_formula != self.byte_beat.next_formula:
+            is_valid, is_byte_beat = self.byte_beat.is_valid_formula(self.byte_beat.next_formula)
+            if is_valid is True:
+                self.byte_beat.current_formula = self.byte_beat.next_formula
+                self.is_byte_beat = is_byte_beat
+                if self.play is False:
+                    self.play = not self.play
+                time.sleep(0)
+                return
+
+        self.play = not self.play
+        time.sleep(0)
+
+    def switch_reverse(self):
+        self.byte_beat.reverse = not self.byte_beat.reverse
+        time.sleep(0)
+
+    def stop_player(self):
+        self.play = False
+        self.byte_beat.reverse = False
+        self.byte_beat.t = 1
+        time.sleep(0)
+
+    def run(self):
+        while True:
+            if self.play is True:
+                self.byte_beat.compute(self.is_byte_beat)
+                self.byte_beat.to_pyaudio(self.is_byte_beat)
+            time.sleep(0)
+
+
+class WorkerSensors(QObject):
     finished = pyqtSignal()
     button_1 = pyqtSignal(bool)
     button_2 = pyqtSignal(bool)
@@ -130,13 +181,6 @@ class ByteBeatUI(qtw.QWidget, Ui_Form):
             [self.db.insert_row(f'slot {i}') for i in range(1, 21)]
         self.slots = self.db.read_all_rows()
 
-        # Playing states
-        self.play = False
-        self.reverse = False
-        self.stop = False
-        self.change_play_state({'stop': self.stop})
-        self.update_right_button({'stop': True, 'pressed': False})
-
         # Set initial button values
         self.button_1.setEnabled(False)
         self.button_2.setEnabled(False)
@@ -159,24 +203,32 @@ class ByteBeatUI(qtw.QWidget, Ui_Form):
         # Start pico read out and set button/dail values
         self.uart = Uart(self.device_file, debug=self.debug)
 
-        # Setup Qthread and worker object
+        # Setup Qthread and worker object for sensor reading
         self.thread = QThread()
-        self.worker = Worker(self.uart, self.debug)
+        self.worker = WorkerSensors(self.uart, self.debug)
         self.get_sensor_data()
+
+        # Setup Qthread and worker object for player
+        self.thread_player = QThread()
+        self.worker_player = WorkerPlayer()
+        self.start_player()
 
         # Connect a return pressed on the line edit to the qlistview
         self.formula_selector_model = QStringListModel(self.slots)
         self.formula_selector.setModel(self.formula_selector_model)
         self.formula_selector.clicked.connect(self.add_selected_formula_to_editor)
         self.formula_editor.returnPressed.connect(self.add_to_formula_selector)
+        self.formula_editor.textChanged.connect(self.check_editted_formula)
         self.formula_selector.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.update_left_button(False, init=True)
 
         # Set input focus on formula-editor (so we don't need an extra computer, but just a keyboard)
         self.formula_editor.setFocus()
 
-        # Setup Byte-beat class instance
-        self.byte_beat_playout = PlayByteBeat(formula='t')
+        # Playing states
+        self.stop = False
+        self.change_play_state({'stop': self.stop})
+        self.update_right_button({'stop': True, 'pressed': False})
 
     def change_play_state(self, status):
         if 'stop' in status:
@@ -204,6 +256,7 @@ class ByteBeatUI(qtw.QWidget, Ui_Form):
     def add_selected_formula_to_editor(self, index):
         text = index.data()
         self.formula_editor.setText(text)
+        self.worker_player.next_formula = text
 
     def add_to_formula_selector(self):
         current_index = self.formula_selector.currentIndex()
@@ -212,9 +265,18 @@ class ByteBeatUI(qtw.QWidget, Ui_Form):
             text = f'slot {current_index.row() + 1}'
             self.formula_editor.setText(text)
         self.formula_selector_model.setData(self.formula_selector_model.index(current_index.row()), text)
+        self.worker_player.byte_beat.next_formula = text
 
         # Save to database
         self.db.update_row(current_index.row() + 1, text)
+
+    def check_editted_formula(self):
+        text = self.formula_editor.text()
+        is_valid, is_byte_beat = self.worker_player.byte_beat.is_valid_formula(text)
+        if is_valid is False:
+            self.formula_editor.setStyleSheet("color: red;")
+        else:
+            self.formula_editor.setStyleSheet("color: black;")
 
     # noinspection PyUnresolvedReferences
     def get_sensor_data(self):
@@ -243,37 +305,50 @@ class ByteBeatUI(qtw.QWidget, Ui_Form):
         # Start the thread
         self.thread.start()
 
+    def start_player(self):
+        # Move worker to the thread
+        self.worker_player.moveToThread(self.thread_player)
+
+        # Connect signals and slots
+        self.thread_player.started.connect(self.worker_player.run)
+        self.worker_player.finished.connect(self.thread_player.quit)
+        self.worker_player.finished.connect(self.worker_player.deleteLater)
+        self.thread_player.finished.connect(self.thread_player.deleteLater)
+
+        # Start the thread
+        self.thread_player.start()
+
     def update_button_1(self, status):
         self.button_1.setEnabled(status)
-        self.byte_beat_playout.OB_value = status
+        self.worker_player.byte_beat.OB_value = status
 
     def update_button_2(self, status):
         self.button_2.setEnabled(status)
-        self.byte_beat_playout.BB_value = status
+        self.worker_player.byte_beat.BB_value = status
 
     def update_button_3(self, status):
         self.button_3.setEnabled(status)
-        self.byte_beat_playout.GB_value = status
+        self.worker_player.byte_beat.GB_value = status
 
     def update_button_4(self, status):
         self.button_4.setEnabled(status)
-        self.byte_beat_playout.RB_value = status
+        self.worker_player.byte_beat.RB_value = status
 
     def update_potmeter_1(self, value):
         self.potmeter_1.setValue(value)
-        self.byte_beat_playout.OP_value = value
+        self.worker_player.byte_beat.OP_value = value
 
     def update_potmeter_2(self, value):
         self.potmeter_2.setValue(value)
-        self.byte_beat_playout.BP_value = value
+        self.worker_player.byte_beat.BP_value = value
 
     def update_potmeter_3(self, value):
         self.potmeter_3.setValue(value)
-        self.byte_beat_playout.GP_value = value
+        self.worker_player.byte_beat.GP_value = value
 
     def update_potmeter_4(self, value):
         self.potmeter_4.setValue(value)
-        self.byte_beat_playout.RP_value = value
+        self.worker_player.byte_beat.RP_value = value
 
     def update_left_button(self, status, init=False):
         self.button_left.setEnabled(status)
@@ -294,23 +369,19 @@ class ByteBeatUI(qtw.QWidget, Ui_Form):
             # Update formula_editor
             selected_text = self.formula_selector_model.data(next_index, 0)
             self.formula_editor.setText(selected_text)
+            self.worker_player.byte_beat.next_formula = selected_text
 
     def update_right_button(self, status):
         self.button_right.setEnabled(status['pressed'])
         if status['pressed'] is False:
-            if status['play_pause']:
-                self.byte_beat_playout.play = not self.byte_beat_playout.play
+            if 'play_pause' in status and status['play_pause']:
+                self.worker_player.switch_play_pause()
 
-            if status['reverse']:
-                self.byte_beat_playout.reverse = True
+            if 'reverse' in status and status['reverse']:
+                self.worker_player.switch_reverse()
 
-            if status['stop']:
-                self.byte_beat_playout.play = False
-                self.byte_beat_playout.reverse = False
-                self.byte_beat_playout.t = 1
-            print(time.time(), status)
-            # play/pause, reverse, stop
-
+            if 'stop' in status and status['stop']:
+                self.worker_player.stop_player()
 
 
 if __name__ == '__main__':
--- QT-ByteBeat/PlayByteBeat.py
+++ QT-ByteBeat/PlayByteBeat.py
@@ -1,16 +1,17 @@
 import math
 import numpy as np
-import threading
+import re
 from pyaudio import PyAudio, paUInt8, paFloat32
 
 
 class PlayByteBeat():
-    def __init__(self, formula='t'):
-        pa = PyAudio()
-        self.audio_int = pa.open(format=paUInt8, channels=1, rate=8000, output=True)
-        self.audio_float = pa.open(format=paFloat32, channels=1, rate=8000, output=True)
-        self.formula = formula
-        self.probe_formula = formula
+    def __init__(self):
+        self.pa = PyAudio()
+        self.audio_int = self.pa.open(format=paUInt8, channels=1, rate=8000, output=True)
+        self.audio_float = self.pa.open(format=paFloat32, channels=1, rate=8000, output=True)
+        self.current_formula = 't'
+        self.next_formula = 't'
+
         self.four_hours = 3600 * 4 * 8000  # 3600 (seconds) * 4 * 8khz samples
         self.t = 1
         self.reverse = False
@@ -26,18 +27,18 @@ class PlayByteBeat():
         self.BB_value = 0
         self.RB_value = 0
         self.GB_value = 0
-        self.thread = threading.Thread(target=self.run)
-        self.thread.start()
-
-    def run(self):
-        while True:
-            valid_formula, is_byte_beat = self.is_valid_formula(self.probe_formula)
-            if valid_formula:
-                self.formula = self.probe_formula
-
-            if not self.stop:
-                self.compute()
-                self.to_pyaudio(is_byte_beat)
+        self.pattern_OPi = re.compile('(OPi)(\[)(\d+)(])')
+        self.pattern_OPf = re.compile('(OPf)(\[)(\d+)(])')
+        self.pattern_BPi = re.compile('(BPi)(\[)(\d+)(])')
+        self.pattern_BPf = re.compile('(BPf)(\[)(\d+)(])')
+        self.pattern_GPi = re.compile('(GPi)(\[)(\d+)(])')
+        self.pattern_GPf = re.compile('(GPf)(\[)(\d+)(])')
+        self.pattern_RPi = re.compile('(RPi)(\[)(\d+)(])')
+        self.pattern_RPf = re.compile('(RPf)(\[)(\d+)(])')
+        self.pattern_OB = re.compile('OB')
+        self.pattern_BB = re.compile('BB')
+        self.pattern_GB = re.compile('GB')
+        self.pattern_RB = re.compile('RB')
 
     def reset_time(self):
         self.t = 1
@@ -54,58 +55,42 @@ class PlayByteBeat():
         :param formula:
         :return: (bool) valid, (bool) is_byte_beat
         """
+        formula = self.replace(formula)
         try:
-            eval(formula, {"t": 1})
+            eval(formula, {'t': 10000})
             return True, True
         except Exception as error:
             self.error = error
 
         try:
-            eval(formula, {
-                "t": 1,
-                "sin": self.sin, "cos": self.cos, "tan": self.tan,
-                "OP8": self.OP8, "OP10":self.OP10, "OP16": self.OP16,
-                "BP8": self.BP8, "BP10":self.BP10, "BP16": self.BP16,
-                "RP8": self.OP8, "RP10":self.OP10, "RP16": self.RP16,
-                "GP8": self.GP8, "GP10":self.GP10, "GP16": self.OP16,
-                "OB": self.OB, "BB": self.BB, "RB": self.RB, "GB": self.GB
-            })
+            eval(formula, {"t": 10000, "sin": self.sin, "cos": self.cos, "tan": self.tan})
             return True, False
         except Exception as error:
             self.error = error
 
         return False, False
 
-    def compute(self):
+    def compute(self, is_byte_beat):
         self.byte_beat_values = []
         if abs(self.t) > self.four_hours:
             self.t = 1
 
-        valid_formula, is_byte_beat = self.is_valid_formula(self.formula)
-        if valid_formula:
-            for _i in range(0x100):
-                try:
-                    if is_byte_beat is True:
-                        value = eval(self.formula, {"t": self.t})
-                        self.byte_beat_values.append(0xFF & value)
-                except Exception as error:
-                    self.error = error
+        formula = self.replace(self.current_formula)
+        for _i in range(0x80):
+            try:
+                if is_byte_beat is True:
+                    value = eval(formula, {'t': self.t})
+                    self.byte_beat_values.append(0xFF & value)
+            except Exception as error:
+                self.error = error
 
-                try:
-                    if is_byte_beat is False:
-                        self.byte_beat_values.append(eval(self.formula, {
-                            "t": self.t,
-                            "sin": self.sin, "cos": self.cos, "tan": self.tan,
-                            "OP8": self.OP8, "OP10":self.OP10, "OP16": self.OP16,
-                            "BP8": self.BP8, "BP10":self.BP10, "BP16": self.BP16,
-                            "RP8": self.OP8, "RP10":self.OP10, "RP16": self.RP16,
-                            "GP8": self.GP8, "GP10":self.GP10, "GP16": self.OP16,
-                            "OB": self.OB, "BB": self.BB, "RB": self.RB, "GB": self.GB
-                        }))
-                except Exception as error:
-                    self.error = error
+            try:
+                if is_byte_beat is False:
+                    self.byte_beat_values.append(eval(formula, {'t': self.t, 'sin': self.sin, 'cos': self.cos, 'tan': self.tan}))
+            except Exception as error:
+                self.error = error
 
-                self.increment()
+            self.increment()
 
     def to_pyaudio(self, is_byte_beat):
         try:
@@ -131,85 +116,70 @@ class PlayByteBeat():
         return math.tan(x)
 
     @staticmethod
-    def OP8(x):
-        normalized_value = (x / 4095) * 8
-        return round(normalized_value)
+    def norm_i(value, factor):
+        """ Normalize value with factor, return int """
+        normalized_value = (((value + 0.000000000001) / 4095) * (factor - 1)) + 1
+        return round(normalized_value)  # Value between 1 and factor
 
     @staticmethod
-    def OP10(x):
-        normalized_value = (x / 4095) * 10
-        return round(normalized_value)
+    def norm_f(value, factor):
+        """ Normalize value with factor, return float """
+        value = round((value + 0.000000000001) / 50) * 50  # 50 is about the average sweep of a potmeter...
+        return ((value + 0.000000000001) / 4095) * factor  # Value between virtually 0 and factor
 
     @staticmethod
-    def OP16(x):
-        normalized_value = (x / 4095) * 16
-        return round(normalized_value)
-
-    @staticmethod
-    def BP8(x):
-        normalized_value = (x / 4095) * 8
-        return round(normalized_value)
-
-    @staticmethod
-    def BP10(x):
-        normalized_value = (x / 4095) * 10
-        return round(normalized_value)
-
-    @staticmethod
-    def BP16(x):
-        normalized_value = (x / 4095) * 16
-        return round(normalized_value)
-
-    @staticmethod
-    def RP8(x):
-        normalized_value = (x / 4095) * 8
-        return round(normalized_value)
-
-    @staticmethod
-    def RP10(x):
-        normalized_value = (x / 4095) * 10
-        return round(normalized_value)
-
-    @staticmethod
-    def RP16(x):
-        normalized_value = (x / 4095) * 16
-        return round(normalized_value)
-
-    @staticmethod
-    def GP8(x):
-        normalized_value = (x / 4095) * 8
-        return round(normalized_value)
-
-    @staticmethod
-    def GP10(x):
-        normalized_value = (x / 4095) * 10
-        return round(normalized_value)
-
-    @staticmethod
-    def GP16(x):
-        normalized_value = (x / 4095) * 16
-        return round(normalized_value)
-
-    @staticmethod
-    def OB(x):
-        if x is False:
-            return 0
+    def button(value):
+        if value is True:
+            return 1
         return 0
 
-    @staticmethod
-    def BB(x):
-        if x is False:
-            return 0
-        return 0
+    def replace(self, formula):
+        # Substitute OPi[n] or OPf[n] for its normalized value based on OP_value and n, respectively for int or float.
+        match = self.pattern_OPi.search(formula)
+        if match:
+            formula = self.pattern_OPi.sub(str(self.norm_i(self.OP_value, float(match.group(3)))), formula)
+        match = self.pattern_OPf.search(formula)
+        if match:
+            formula = self.pattern_OPf.sub(str(self.norm_f(self.OP_value, float(match.group(3)))), formula)
 
-    @staticmethod
-    def RB(x):
-        if x is False:
-            return 0
-        return 0
+        # Substitute BPi[n] or BPf[n] for its normalized value based on BP_value and n, respectively for int or float.
+        match = self.pattern_BPi.search(formula)
+        if match:
+            formula = self.pattern_BPi.sub(str(self.norm_i(self.BP_value, float(match.group(3)))), formula)
+        match = self.pattern_BPf.search(formula)
+        if match:
+            formula = self.pattern_BPf.sub(str(self.norm_f(self.BP_value, float(match.group(3)))), formula)
 
-    @staticmethod
-    def GB(x):
-        if x is False:
-            return 0
-        return 0
+        # Substitute GPi[n] or GPf[n] for its normalized value based on GP_value and n, respectively for int or float.
+        match = self.pattern_GPi.search(formula)
+        if match:
+            formula = self.pattern_GPi.sub(str(self.norm_i(self.GP_value, float(match.group(3)))), formula)
+        match = self.pattern_GPf.search(formula)
+        if match:
+            formula = self.pattern_GPf.sub(str(self.norm_f(self.GP_value, float(match.group(3)))), formula)
+
+        # Substitute RPi[n] or RPf[n] for its normalized value based on RP_value and n, respectively for int or float.
+        match = self.pattern_RPi.search(formula)
+        if match:
+            formula = self.pattern_RPi.sub(str(self.norm_i(self.RP_value, float(match.group(3)))), formula)
+        match = self.pattern_RPf.search(formula)
+        if match:
+            formula = self.pattern_RPf.sub(str(self.norm_f(self.RP_value, float(match.group(3)))), formula)
+
+        match = self.pattern_OB.search(formula)
+        if match:
+            formula = self.pattern_OB.sub(str(self.button(self.OB_value)), formula)
+
+        match = self.pattern_BB.search(formula)
+        if match:
+            formula = self.pattern_BB.sub(str(self.button(self.BB_value)), formula)
+
+        match = self.pattern_GB.search(formula)
+        if match:
+            formula = self.pattern_GB.sub(str(self.button(self.GB_value)), formula)
+
+        match = self.pattern_RB.search(formula)
+        if match:
+            formula = self.pattern_RB.sub(str(self.button(self.RB_value)), formula)
+
+        return formula
